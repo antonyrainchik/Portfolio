3d34228df583d197cf138e36c25a11758a08abed
antonyrainchik <83198700+antonyrainchik@users.noreply.github.com>
2023-11-27 14:08:03 -0600

    If it was just the /

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio',
+  base: '/Portfolio/',
   server: {
     // Set host to true to expose the server to your local network
     host: true
