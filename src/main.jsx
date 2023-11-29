513a6b3b3ffe162ba98779ea4bcc41c961eed781
antonyrainchik <83198700+antonyrainchik@users.noreply.github.com>
2023-11-29 14:06:12 -0600

    nothing is working :(

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import reportWebVitals from "./reportWebVitals";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+reportWebVitals();
