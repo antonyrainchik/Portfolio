dcfeb7a6a737bccabcc5f6a5e480bf6bb2179002
antonyrainchik <83198700+antonyrainchik@users.noreply.github.com>
2024-09-10 13:48:45 -0500

    Revert "This might break things, but I am trying to remove the intro"
    This reverts commit 3de8779f6cd93e1696674d8c5f5638751475940f.

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,26 +2,26 @@ import React, { useEffect, useState } from 'react';
 import '../Intro.css';
 import {GoGear} from 'react-icons/go'
 
-// const Intro = ({ onFadeComplete, backgroundImage }) => {
-//   const [fadeOut, setFadeOut] = useState(false);
+const Intro = ({ onFadeComplete, backgroundImage }) => {
+  const [fadeOut, setFadeOut] = useState(false);
 
-//   useEffect(() => {
-//     // Set a timeout to fade out the intro after a certain time period
-//     const timer = setTimeout(() => setFadeOut(true), 0); // 0 seconds, might be easier than deleting
+  useEffect(() => {
+    // Set a timeout to fade out the intro after a certain time period
+    const timer = setTimeout(() => setFadeOut(true), 0); // 0 seconds, might be easier than deleting
 
-//     return () => clearTimeout(timer);
-//   }, []);
+    return () => clearTimeout(timer);
+  }, []);
 
-//   useEffect(() => {
-//     if (fadeOut) {
-//       // When fade out starts, set another timeout to remove the intro
-//       const timer = setTimeout(() => {
-//         onFadeComplete();
-//       }, 0); 
+  useEffect(() => {
+    if (fadeOut) {
+      // When fade out starts, set another timeout to remove the intro
+      const timer = setTimeout(() => {
+        onFadeComplete();
+      }, 0); 
 
-//       return () => clearTimeout(timer);
-//     }
-//   }, [fadeOut, onFadeComplete]);
+      return () => clearTimeout(timer);
+    }
+  }, [fadeOut, onFadeComplete]);
 
   return (
     // <div className={`intro ${fadeOut ? 'fade-out' : ''}`} style={{ backgroundImage: `url(${backgroundImage})`} }>
@@ -33,6 +33,6 @@ import {GoGear} from 'react-icons/go'
     // </div>
     <div></div>
   );
-// };
+};
 
 export default Intro;
